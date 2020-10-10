b3efece5f4b168b897523149ac62c87249212d5f
Dmirty <dmitry.ml@mail.ru>
2020-10-10 17:14:25 +0300

    new

--- controller.py
+++ controller.py
@@ -1,34 +1,36 @@
 from setting import OK, NOT_FOUND
-from template_engine import render
-from utilities import Request, Response
+from templates.template import render
+from utilities import Response
 
 
 def main_view(request, method, request_params):
     data = request.datas.get('data', None)
     title = 'Главна'
-    content = f'это {method} запрос c параметрами = {request_params}'
-    renders = render('templates/authors.html', object_list=[{'title': title}, {'content': content}, {'data': data}])
+    content = 'Текс'
+    renders = render('index.html', object_list=[{'title': title}, {'content': content}, {'data': data}])
     return Response(OK, [renders])
 
 
 def about_view(request, method, request_params):
     data = request.datas.get('data', None)
     title = 'О нас'
-    content = f'это {method} запрос c параметрами = {request_params}'
-    renders = render('templates/authors.html', object_list=[{'title': title}, {'content': content}, {'data': data}])
-    return Response(OK, [renders])
+    content = 'Текс'
+    renders = render('authors.html', object_list=[{'title': title}, {'content': content}, {'data': data}])
+    return Response(OK, [renders], )
 
 
 def contact_view(request, method, request_params):
+    if method == 'POST':
+        print(request_params) # работаем с введенными параметрами
     data = request.datas.get('data', None)
     title = 'Контакты'
     content = f'это {method} запрос c параметрами = {request_params}'
-    renders = render('templates/contact.html', object_list=[{'title': title}, {'content': content}, {'data': data}])
+    renders = render('contact.html', object_list=[{'title': title}, {'content': content}, {'data': data}])
     return Response(OK, [renders])
 
 
 def view_404(request):
     title = 'Not_Found'
     content = 'Не найдено'
-    renders = render('templates/authors.html', object_list=[{'title': title}, {'content': content}])
+    renders = render('authors.html', object_list=[{'title': title}, {'content': content}])
     return Response(NOT_FOUND, [renders])
